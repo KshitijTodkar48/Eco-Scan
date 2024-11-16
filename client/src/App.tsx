f9c77a13e4b6c9d7d86927f8b8f5e1e773e178c3
Kshitij Todkar <kshitijtodkar4826@gmail.com>
2024-11-16 13:31:02 +0530

    initialized frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() { 
+  return (
+    <div className="border-2 border-green-500 text-4xl text-center py-4 bg-green-200 font-semibold">
+      Eco-Scan
+    </div>
+  )
+}
+
+export default App
